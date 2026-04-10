be46e06fb99fb6e22ff5c4728169788daf1c166b
cbaptiste <baptiste.chevrin@ynov.com>
2026-04-10 15:14:29 +0200

    JavaProject

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+package src.main.java;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        // primaryStage = la fenêtre principale de votre app
+        
+        // On affiche le menu principal
+        MenuPrincipal menu = new MenuPrincipal(primaryStage);
+        menu.afficher();
+        
+        // Configuration de la fenêtre
+        primaryStage.setTitle("Data & IA Toolbox");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args); // Démarre l'application JavaFX
+    }
+}
